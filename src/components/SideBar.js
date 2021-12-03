d2e301afa4d02432a6585e3f8ac40d9100008df9
Jothika R <jothikajoe2411@gmail.com>
2021-12-03 16:42:40 +0530

    color

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,37 +1,50 @@
 import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
-import Dashboard from "./assets/Dashboard.svg";
-import AssignmentRules from "./assets/AssignmentRules.svg";
-import Tickets from "./assets/Tickets.svg";
-import Logout from "./assets/Logout.svg";
+import Dashboard from "../assets/Dashboard.svg";
+import AssignmentRules from "../assets/AssignmentRules.svg";
+import Tickets from "../assets/Tickets.svg";
+import Logout from "../assets/Logout.svg";
 
-function SideBar() {
+const SideBar = ({ history }) => {
+  console.log(history);
+  const getColor = (curr) => {
+    if (history.location.pathname === curr) {
+      return "#FFFFFF";
+    }
+  };
+
   return (
     <nav>
       <div className="sideBar">
         <div className="icon">
-          <Link to="/">
+          <Link to="/" style={{ backgroundColor: getColor("./") }}>
             <img src={Dashboard} alt="Dashboard" />
           </Link>
         </div>
         <div className="icon">
-          <Link to="/contact">
+          <Link
+            to="/contact"
+            style={{ backgroundColor: getColor("./contact") }}
+          >
             <img src={AssignmentRules} alt="AssigmentRules" />
           </Link>
         </div>
         <div className="icon">
-          <Link to="/profile">
+          <Link
+            to="/profile"
+            style={{ backgroundColor: getColor("./profile") }}
+          >
             <img src={Tickets} alt="Tickets" />
           </Link>
         </div>
         <div className="icon">
-          <Link to="/share">
+          <Link to="/share" style={{ backgroundColor: getColor("./share") }}>
             <img src={Logout} alt="Logout" />
           </Link>
         </div>
       </div>
     </nav>
   );
-}
+};
 export default SideBar;
